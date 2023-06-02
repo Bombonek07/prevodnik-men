d8ba064f4b5e092755dd76a8425c3d3d52d6f43c
Bombonek07 <ondrabondor07@gmail.com>
2023-06-02 19:41:00 +0200

    3commit

--- function.js
+++ function.js
@@ -1,6 +1,5 @@
-let pre_z = 0
-let pre_do = 0
-
+let pre_z;
+let pre_do;
 
 
 function currency() { 
@@ -15,8 +14,31 @@ async function input(){
     let num = document.getElementById("input").value;
     let raw = await fetch("https://data.kurzy.cz/json/meny/b[6].json")
     let data = await raw.json()
-    console.log(data)
+    let kurz = 0
+    let kurz2 = 0
+    if (pre_z === "CZK"){
+        kurz = 1
+        document.getElementById("alert").innerHTML = ""
+    } else if (pre_z === "null") {
+        document.getElementById("alert").innerHTML = "VYBERTE MĚNU"
+        document.getElementById("num").innerHTML = ""
+        return;
+    } else {
+        kurz = data["kurzy"][pre_z]["dev_stred"]
+        document.getElementById("alert").innerHTML = ""
+    }
+
+    if (pre_do === "CZK"){
+        kurz2 = 1
+        document.getElementById("alert").innerHTML = ""
+    } else if (pre_do === "null") {
+        document.getElementById("alert").innerHTML = "VYBERTE MĚNU"
+        document.getElementById("num").innerHTML = ""
+        return;
+    } else {
+        kurz2 = data["kurzy"][pre_do]["dev_stred"]
+        document.getElementById("alert").innerHTML = ""
+    }
     
-    /*document.getElementById("num").innerHTML = num;
-    console.log(num)*/
+    document.getElementById("num").innerHTML = num/kurz*kurz2;
 }
