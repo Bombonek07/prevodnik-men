685c58f9ac7b5ac51a3dcffa26af46fad3bb668d
Bombonek07 <ondrabondor07@gmail.com>
2023-06-02 16:19:09 +0200

    1commit

---
+++ function.js
@@ -0,0 +1,15 @@
+function lenguage() { 
+    var result = document.getElementById("option").value; 
+    console.log(result)
+}
+
+function lenguage2() { 
+    var result2 = document.getElementById("option2").value; 
+    console.log(result2)
+}
+
+function input(){
+    var num = document.getElementById("input").value;
+    document.getElementById("num").innerHTML = num;
+    console.log(num)
+}
